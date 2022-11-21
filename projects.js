c226741eabba8a4fc08cd162b862c23a43fedd9f
jothi lakshmi <96345381+jothilakshmi22@users.noreply.github.com>
2022-11-21 05:20:45 -1200

    delete commants

--- projects.js
+++ projects.js
@@ -10,37 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let color = document.getElementById("colorpicker");
   let bordersize = document.getElementById("bordersize");
 
-  ///!!!!!!!!!!unproper code
-  //   range.value = "50";
-  //   const allborderupdated = () => {
-  //     const pxvalue = (span.innerText = `${range.value}px`);
-  //     // var coding = `border-radius:"${pxvalue}" border:"4px ${selectvalue} black" `;
-  //     // let textboxvalue = (textbox.innerHTML = coding);
-  //     container.style.borderRadius = pxvalue;
-  //     // container.style.border = "1px  black";
-  //     select.addEventListener("change", () => {
-  //       let select = document.getElementById("select");
-  //       let selectvalue = select.value;
-  //       let color = "red";
-  //       container.style.border = `"4px ${selectvalue} ${color}"`;
-  //       //   //   color picker
-  //       //   color.addEventListener("change", () => {
-  //       //     colorvalue = color.value;
-  //       //   });
-
-  //       var coding = `border-radius:"${pxvalue}" border:"4px ${selectvalue} black"`;
-  //       let textboxvalue = (textbox.innerHTML = coding);
-  //       // alert("hi");
-  //     });
-  //   };
-
-  // ------------------------
-
-  //   select.addEventListener("change", () => {
-  //     let select = document.getElementById("select");
-  //     let selectvalue = select.value;
-  //     container.style.border = `1px ${selectvalue} black`;
-  //     // alert("hi");
+ 
   var coding = "";
   borderstyle = "solid";
   bordercolor = "blue";
